3de4f0d9d066f07a7307deca7cf552049f4479b8
Prieto-Mayca-A <68166806+Prieto-Mayca-A@users.noreply.github.com>
2021-04-14 23:22:31 -0500

    quinto commit

--- guiat/src/App.js
+++ guiat/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const eventoName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const eventoClick = (event) => {
+    
+
+    event.preventDefault()
+    const personarepe = {name: newName}
+    setPersons(
+      [...persons, personarepe]
+      )
+
+    var cont=1 
+
+    for (var k = 0; k < persons.length; k++) {
+      if(persons[k].name === personarepe.name){
+        cont++
+      } 
+    }
+    var NewName = personarepe.name
+    if(cont === 1){
+      setPersons([...persons, personarepe])
+      window.alert('El nombre '+NewName+' a sido agregado :)');
+      setNewName("")
+    } else {
+      window.alert('El nombre ' +NewName+' esta repetido');
+      setNewName("")
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input type="text" value={newName} onChange={eventoName} />
+          <p></p>
+        </div>
+        <div>
+          <button type="submit"  onClick={eventoClick}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <h2>Los names de todas las personas:</h2>
+          <p></p>
+          {persons.map((personanueva,index) => {return(<li key={index}>{personanueva.name}</li>)})}
     </div>
   );
 }
 
 export default App;
+
